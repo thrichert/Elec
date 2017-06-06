a25024d30add0f3bc11c90f512c0a59a20e642fb
AntoHesse <antoine.riard@gmail.com>
2017-06-06 15:44:16 +0200

    electro pontH

--- Firmware ROBIN/PontH/pont.c
+++ Firmware ROBIN/PontH/pont.c
@@ -1,22 +1,56 @@
-void	ConfigurePontH(void)
+void	ConfigureHBridge(void)
 {
-	TRISxbits.TRISFx = 0;
-	TRISxbits/TRISFx = 0;
+	TRISbits.TRIS = 1;
+	TRISbits.TRIS = 1;
 }
 
-void	Wakeup_Hbridge(void)
+void	OpenEV(void)
 {
-	;
+	LATbits.LAT = 1;
+	LATbits.LAT = 1;
+	//SecondPinHigh
+	//Watchdog
+	LATbits.LAT = 0;
+	LATbits.LAT = 0;
+
+//	PH = 1;
+//	EN = 1;
+//	Wait();
+//	PH = 0;
+//	EN = 0;
 }
 
-void	Sleep_Hbridge(void)
+void	CloseEV(void)
 {
-	;
+	LATbits.LAT = 1: //reverse
+	LATbits.LAT = 0;
+	//Watchdog
+	LATbits.LAT = 0;
+	LATbits.LAT = 0;
+
+//	PH = 1; //reverse
+//	EN = 0; 
+//	Wait();
+//	PH = 0;
+//	EN = 0;
 }
 
-void	TransmitPulsation(void)
+int	main(void)
 {
-	CloseBridge;
-	TimeMicroSecs;
-	OpenBridge;
+	while (1)
+	{
+		if (flag_user_eau == 1 || DFT > 86400)
+		{
+			if (!EV)
+				OpenEV();
+			StartComfortTimer();
+			StartSecurityTimer();	
+		}
+		if (flag_user_eau == 0 && (flag_comfort_timer == 1 
+			|| flag_security_timer == 1 || user_soap == 1))
+		{
+			CloseEV();
+			StartDFtimer();
+		}
+	}
 }
